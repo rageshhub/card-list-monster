d31b4482696f5cd3130648f82bae75e11740de8f
Ragesh G <snrgrageshcv@gmail.com>
2020-06-13 20:22:59 +0530

    Updates

---
+++ precache-manifest.600964d9b5e664bc714e5a51851a0f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3241b0ac3ca7803435ee91ef417bf8",
+    "url": "/card-lister-monster/index.html"
+  },
+  {
+    "revision": "f85325f36675e1ae196e",
+    "url": "/card-lister-monster/static/css/main.cbde4794.chunk.css"
+  },
+  {
+    "revision": "93def368ba2567f23b14",
+    "url": "/card-lister-monster/static/js/2.16eb62f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-lister-monster/static/js/2.16eb62f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85325f36675e1ae196e",
+    "url": "/card-lister-monster/static/js/main.a00531d9.chunk.js"
+  },
+  {
+    "revision": "af13cc8feb668cd942d9",
+    "url": "/card-lister-monster/static/js/runtime-main.7a34c2d3.js"
+  }
+]);
